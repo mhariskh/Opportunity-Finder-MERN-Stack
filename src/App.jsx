ffb37a61023eaa7b33685837174d0a937e7dd107
Muhammad Haris Khan <mhkhan.bscs19seecs@seecs.edu.pk>
2022-03-08 21:35:10 +0500

    Initialization Done in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>  Project Opportunity Finder Has Been Initialized </h2>
+    
+    </div>
+  );
+}
+
+export default App;
